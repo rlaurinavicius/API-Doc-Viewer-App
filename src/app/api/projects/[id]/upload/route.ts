2a04d617807959ce2fea2f1cf0a480bf84899047
rlaurinavicius <romualdaslaur@gmail.com>
2025-08-31 19:24:42 +0300

    refactor: use singleton PrismaClient instance

--- src/app/api/projects/[id]/upload/route.ts
+++ src/app/api/projects/[id]/upload/route.ts
@@ -1,9 +1,7 @@
 
 import { NextResponse } from "next/server";
 import * as jsyaml from "js-yaml";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma";
 
 export async function POST(request: Request, { params }: { params: { id: string } }) {
   const projectId = params.id;
